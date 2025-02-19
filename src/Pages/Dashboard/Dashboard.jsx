ad3fb10680ee41b0b75d82745bd71d52a1c9d0e0
abubakirilhomov <2ilhomovabubakir2@gmail.com>
2025-02-19 22:35:32 +0500

    api url

--- src/Pages/Dashboard/Dashboard.jsx
+++ src/Pages/Dashboard/Dashboard.jsx
@@ -1,11 +1,24 @@
-import React from 'react'
+import React from "react";
+import { useSelector } from "react-redux";
+import { motion } from "framer-motion";
 
 const Dashboard = () => {
+  const user = useSelector((state) => state.auth.user.username);
+  console.log(user);
   return (
     <div>
+      <motion.p
+        initial={{ opacity: 0, scale: 0.8 }}
+        animate={{ opacity: 1, scale: 1 }}
+        exit={{ opacity: 0, scale: 0.8 }}
+        transition={{ duration: 0.5, ease: "easeOut" }}
+        className="bg-clip-text text-transparent bg-gradient-to-r from-info via-secondary to-accent text-5xl font-bold"
+      >
+        Welcome <span className="bg-clip-text text-transparent bg-gradient-to-r from-primary to-info text-5xl font-bold">{user}</span>
+      </motion.p>
       Dashboard
     </div>
-  )
-}
+  );
+};
 
-export default Dashboard
+export default Dashboard;
