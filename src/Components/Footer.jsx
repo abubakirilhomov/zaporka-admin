ddeb55ec8222f30f78fa92ddff790389747a6f78
navruzchik-dev <navruzmurodullaev1@gmail.com>
2025-03-01 16:17:02 +0500

    asdfghjkl

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => (
   <div className="absolute bottom-0 max-w-[80%] right-0 w-full">
     <p className="dark:text-gray-200 text-gray-700 text-center">
-      © 2022 All rights reserved by Shoppy.com
+    
     </p>
   </div>
 );
